eecee14dc2a28dce4d68a47ae1b50fd17ada4b15
alexandercastillo <alex.castillo@door3.com>
2015-01-25 12:13:52 -0500

    results conditionals

--- pages/results/results.ctrl.js
+++ pages/results/results.ctrl.js
@@ -1,5 +1,13 @@
 'use strict'
 
-App.controller('resultsCtrl', function ($scope) {
+App.controller('resultsCtrl', function ($scope, $sessionStorage) {
+
+  $scope.$storage = $sessionStorage.$default({
+    results: {}
+  });
+
+  angular.forEach($scope.$storage.questions, function (question) {
+    $scope.$storage.results[question.id] = question.answer;
+  });
 
 });
